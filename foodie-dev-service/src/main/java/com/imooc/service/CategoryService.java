eae4fc85e260af9e1366ac9b8eec361ac2a1ab1c
iiiIgnorance <zl3234@columbia.edu>
2023-04-04 23:45:03 -0400

    add item

--- foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.imooc.service;
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
 import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
 
 import java.util.List;
 
@@ -19,6 +20,14 @@ public interface CategoryService {
      * @return
      */
     public List<CategoryVO> getSubCatList(Integer rootCatId);
+
+    /**
+     * 查询首页每个一级分类下的6条最新商品数据
+     * @param rootCatId
+     * @return
+     */
+
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
 
 
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -14,7 +14,9 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -39,5 +41,13 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<CategoryVO> getSubCatList(Integer rootCatId) {
         return categoryMapperCustom.getSubCatList(rootCatId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List getSixNewItemsLazy(Integer rootCatId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("rootCatId", rootCatId);
+        return categoryMapperCustom.getSixNewItemsLazy(map);
     }
 }
